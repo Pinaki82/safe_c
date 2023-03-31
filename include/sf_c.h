ba463564704d578b0fda45abc40405e9fb956064
Pinaki Gupta <appugupta007@gmail.com>
2023-03-31 13:34:46 +0530

    1. Added a function sf_strlen() (re-implementation) that can deal with 1x10^+5 = 1 M chars. 2. used that sf_strlen() in the header itself, size_t len = sf_strlen(str, MAXBUFF); 3. define a macro #define strlen sf_strlen to prevent accedental use of strlen()

--- include/sf_c.h
+++ include/sf_c.h
@@ -8,7 +8,7 @@
 
 
 //MACRO, global variables, etc..
-
+#define MAXBUFF 1E+5d   // integer 1x10^+5 = 1 M. the max no. chars that will be dealt with.
 
 
 #include <math.h>
@@ -16,12 +16,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// https://www.tutorialspoint.com/c_standard_library/c_function_strncpy.htm
+// an alternative function to strlen() that checks buffer size as an argument
+size_t sf_strlen(const char *str, size_t max_len);
 
+// https://www.tutorialspoint.com/c_standard_library/c_function_strncpy.htm
 
 // an alternative function to strcpy() that checks buffer size as an argument
 void sf_strcpy(char *dest, const char *src, size_t size);  //size=sizeof(dest). Increase size at declaration.
@@ -32,11 +38,48 @@ void sf_strncpy(char *dest, const char *src, size_t n);
 // an alternative function to gets() that checks buffer size as an argument
 char *sf_gets(char *str, int size, FILE *stream);
 
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif  /* __SF_C_H__ */
+
+// an alternative function to strlen() that checks buffer size as an argument
+// Based on // https://stackoverflow.com/questions/5935413/is-there-a-safe-version-of-strlen
+// Minor changes has been made with the help of ChatGPT and efforts on my part.
+/* // https://stackoverflow.com/questions/32599064/is-strlen-unsafe-when-getting-the-last-char-in-a-string-in-c
+   // Now, the following will be handled perfectly.
+  size_t max_len = 15;
+  size_t len;
+  char *str = (char *)NULL;
+  len = sf_strlen2(str, max_len);  // no segmentation fault -- no crash
+*/
+size_t sf_strlen(const char *str, size_t max_len) {
+  if(str == NULL) {
+    // handle null string case
+    fprintf(stderr, "%s", "null string!\n");
+    return 0;
+  }
+
+  else if(max_len == 0) {
+    // handle zero length case
+    return 0;
+  }
+
+  else {
+    const char *end = (const char *)memchr(str, '\0', max_len);
+
+    if(end == NULL) {
+      // string is longer than max_len
+      return max_len - 1;
+    }
+
+    else {
+      return end - str;
+    }
+  }
+}
 
 // an alternative function to strcpy() that checks buffer size as an argument
 void sf_strcpy(char *dest, const char *src, size_t size) { //size=sizeof(dest). Increase size at declaration.
@@ -85,7 +128,7 @@ char *sf_gets(char *str, int size, FILE *stream) { //size=sizeof(dest).
     return NULL;
   }
 
-  size_t len = strlen(str); //strlen() returns the length of the string in bytes
+  size_t len = sf_strlen(str, MAXBUFF); //strlen() returns the length of the string in bytes
 
   if(len > 0 && str[len - 1] == '\n') { //if the last character of the string is a newline
     str[len - 1] = '\0'; //then remove it
@@ -98,6 +141,7 @@ char *sf_gets(char *str, int size, FILE *stream) { //size=sizeof(dest).
 // strncpy, and gets from the compiler provided library anymore
 // because they are now macros and they are re-defined here.
 
+#define strlen sf_strlen
 #define strcpy sf_strcpy
 #define strncpy sf_strncpy
 #define gets sf_gets
